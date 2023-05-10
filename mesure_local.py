377d34f8e83b68f594330eb6909c78feca709064
EL KHADER mohamed_Reda <melkhader@enseirb-matmeca.fr>
2023-05-10 21:33:04 +0200

    modif

--- mesure_local.py
+++ mesure_local.py
@@ -1,48 +1,85 @@
-def vertex_neighbors(s, Mat) :
-    T=[]
-    for i in range(len(Mat)):
-        if Mat[s][i] != 0:
-            T.append(i)
-    return T 
-
-def test_vertex_neighbors():
-    print(vertex_neighbors(6, Coactivation_matrix1))
-# test_vertex_neighbors()
+import random
+import numpy as np
+import time
+import matplotlib.pyplot as plt
 
 
-def Kth_neighbor (Mat ,sommet ,k):
-    "Cette fonction retourne l ' ensemble des voisins distance k du sommet"
-    count = 1
-    result_set = vertex_neighbors(sommet, Mat)
-    result1 = []
-    while(count < k ): #count est la profondeur du parcours
-        for elt in result: 
-            result1 += vertex_neighbors (elt, Mat) #les voisins des voisins
-            result1 = list(set(result1 )) #on supprime les doublons
-        result_set = result1 #on rassemble les sommets visités
-        count += 1 #on descend 
-    return result_set
-
-def topological_overlap (Mat ,s1 ,s2 ,k):
-    list1 = Kth_neighbor (Mat ,s1 ,k)
-    list2 = Kth_neighbor (Mat ,s2 ,k)
-    return list(set(list1) & set(list2 )) # n retourne l'intersection des deux ensembles sous forme de liste
+"Cette fonction retourne les voisins du sommet"
+def array_of_neighbors (matrix , sommet ):
+    neighbors = []
+    for i in range( matrix.shape[0]):
+        if ( matrix[ sommet ] [ i ] != 0):
+            neighbors.append(i)
+    return neighbors
 
 
-def clusetring_coefficient_vertex (Mat ,sommet ):
-    "Cette fonction renvoie le coefficient de clusetring d ' un sommet"
-    neighbors = vertex_neighbors (sommet, Mat)
+"Cette fonction retourne l'ensemble des voisins distance k du sommet"
+def k_neighbors(matrix, sommet, k):
+    distance = 1
+    neighbors = array_of_neighbors(matrix, sommet)
+    new_neighbors = []
+
+    while(distance < k):
+        for neighbor in neighbors:
+            new_neighbors += array_of_neighbors (matrix , neighbor)
+            new_neighbors = list(set(new_neighbors))
+        neighobrs = new_neighbors
+        distance += 1
+    return neighbors
+
+"cette fonction calcule le chevauchement topologique de deux sommets d’un graphe"
+def topological_overlap (matrix ,S1 ,S2 ,k):
+    neighbors1 = k_neighbors (matrix ,S1 ,k)
+    neighbors2 = k_neighbors (matrix ,S2 ,k)
+    intersection= list(set(neighbors1) & set(neighbors2))
+    return len(intersection)
+
+
+
+def generate_random_matrix(size, min_value=0, max_value=100):
+    return np.array([[random.randint(min_value, max_value) for _ in range(size)] for _ in range(size)])
+
+
+def plot_topological_overlap_time():
+    sizes = list(range(100, 600, 10))
+    k_values = [1, 2, 5, 10]  # Valeurs de k choisies pour l'exemple
+    plt.figure()
+    for k in k_values:
+        times = []
+        for size in sizes:
+            matrix = generate_random_matrix(size)
+            S1, S2 = 0,1
+            start_time = time.time()
+            topological_overlap(matrix, S1, S2, k)
+            end_time = time.time()
+            times.append(end_time - start_time)
+
+        plt.semilogy(sizes, times, label=f'k={k}')
+    plt.xlabel('Nmax')
+    plt.ylabel('Temps (secondes)')
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+plot_topological_overlap_time()
+
+
+"Cette fonction renvoie le coefficient de clusetring d'un sommet"
+def clusetring_coefficient_of_vertex (matrix ,sommet ):
+    neighbors = array_of_neighbors (matrix , sommet)
     count = 0
-    for elt1 in neighbors :
-        for elt2 in neighbors :
-             if Mat[elt1][elt2] != 0:
-                count+=1
-    return count /(len( neighbors ) * (len( neighbors)-1))
+    for neighbor1 in neighbors :
+        for neighbor2 in neighbors :
+            if matrix [neighbor1 ][neighbor2] != 0 :
+                count += 1
+    return count /( len( neighbors ) * (len( neighbors)-1))
 
+
+"Cette fonction revoie le coefficient de clusetring totale d'un graphe "
 def clusetring_coefficient_graph (Mat):
     clustring_total = 0
     for i in range (len(Mat)):
-        clustring_total += clusetring_coefficient_vertex(Mat, i)
+        clustring_total += clusetring_coefficient_of_vertex(Mat, i)
     return clustring_total/ len(Mat)
 
 
@@ -54,6 +91,7 @@ def nbAreteGraphe(Mat):
             if Mat[i][j] !=0:
                 count+=1
     return count
+
 def modularity (matrix , community ):
     "Cette fonction renvoie la m o d u l a r i t de la partition community"
     n = len(matrix)
